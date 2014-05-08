d314aca8bb42f3c5c93ce53b0f51185b3f233499
Asher Cohen <asherlc@asherlc.com>
2014-05-07 21:24:00 -0700

    more

--- cookbooks/vocalem/recipes/default.rb
+++ cookbooks/vocalem/recipes/default.rb
@@ -2,17 +2,16 @@
 # Cookbook Name:: vocalem
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, East Bay Software
 #
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'build-essential::default'
+
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
-
-# Ruby stuff
 rbenv_ruby "2.1.1"
-
 rbenv_gem "bundler" do
   ruby_version "2.1.1"
 end
@@ -20,4 +19,12 @@ end
 include Opscode::OpenSSL::Password
 set_unless[:my_password] = secure_password
 
-
+include_recipe "database::postgresql"
+postgresql_database 'vocalem_production' do
+  connection(
+    :host      => '127.0.0.1'
+    :port      => 5432,
+    :username  => 'deploy',
+  )
+  action :create
+end
